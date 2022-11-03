00f330fd55dbd97fa80b58c3219c16b8cbca4dd9
Nesreengdb <nesreenhamad25@gmail.com>
2022-11-03 15:44:45 +0300

    oun.sql export database

---
+++ oun.sql
@@ -0,0 +1,184 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 03, 2022 at 01:27 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `oun`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `babysitter`
+--
+
+CREATE TABLE `babysitter` (
+  `name` varchar(20) NOT NULL,
+  `national_ID` int(10) NOT NULL,
+  `phoneNo` int(10) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `gender` enum('Female','Male') NOT NULL,
+  `age` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `city` varchar(20) NOT NULL,
+  `major` varchar(30) NOT NULL,
+  `academic_qual` enum('bachelors','masters','phd') NOT NULL,
+  `experience_years` int(11) NOT NULL,
+  `skills` varchar(100) NOT NULL,
+  `languages` varchar(100) NOT NULL,
+  `extra_info` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
+
+CREATE TABLE `bookings` (
+  `booking_id` int(10) NOT NULL,
+  `order_id` int(10) NOT NULL,
+  `kid_id` int(10) NOT NULL,
+  `review` varchar(500) NOT NULL,
+  `duration` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `babysitter_id` int(10) NOT NULL,
+  `parent_id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `children`
+--
+
+CREATE TABLE `children` (
+  `kid_id` int(10) NOT NULL,
+  `parent_id` int(10) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `gender` enum('Female','Male') NOT NULL,
+  `age` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(10) NOT NULL,
+  `price` float NOT NULL,
+  `service_type` varchar(20) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `duration` decimal(10,0) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parent`
+--
+
+CREATE TABLE `parent` (
+  `parent_image` blob NOT NULL,
+  `parent_id` int(10) NOT NULL,
+  `name` int(30) NOT NULL,
+  `kid_id` int(10) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `city` varchar(20) NOT NULL,
+  `district` varchar(20) NOT NULL,
+  `street` varchar(20) NOT NULL,
+  `buildingNo` varchar(20) NOT NULL,
+  `phoneNo` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `babysitter`
+--
+ALTER TABLE `babysitter`
+  ADD PRIMARY KEY (`national_ID`);
+
+--
+-- Indexes for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD PRIMARY KEY (`booking_id`);
+
+--
+-- Indexes for table `children`
+--
+ALTER TABLE `children`
+  ADD PRIMARY KEY (`kid_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indexes for table `parent`
+--
+ALTER TABLE `parent`
+  ADD PRIMARY KEY (`parent_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `babysitter`
+--
+ALTER TABLE `babysitter`
+  MODIFY `national_ID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `bookings`
+--
+ALTER TABLE `bookings`
+  MODIFY `booking_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `children`
+--
+ALTER TABLE `children`
+  MODIFY `kid_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `parent`
+--
+ALTER TABLE `parent`
+  MODIFY `parent_id` int(10) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
