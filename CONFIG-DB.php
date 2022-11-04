e52e40becbc7c3d40d7cf9ad171b36198682ec24
sarahdoi <112553205+sarahdoi@users.noreply.github.com>
2022-11-04 21:14:28 +0300

    test.php

--- CONFIG-DB.php
+++ CONFIG-DB.php
@@ -5,9 +5,4 @@
     define("DBNAME","oun");
 
 
-    $con = mysqli_connect( DBHOST , DBUSER , DBPWD , DBNAME );
-    if (!$con )
-    die('can not connect to db'.mysql_error());
-    else
-    echo('connection to db is successful');
 ?>
