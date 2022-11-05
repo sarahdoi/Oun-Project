d0a4bb85516057b828bfd451ee937f21bc9ad082
taiif <teef.200@hotmail.com>
2022-11-05 15:02:56 +0300

    profile pic update,,
    it saves the uploaded pic in images folder with unique id+the name (target path) as in data base.. to retrive it just retrive in src= images( path from db)

--- parentReg.php
+++ parentReg.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-
+//if(isset($_POST['submit'])) must add i think?
 //initiaising 
 // $email = "";
 $errors = array();
@@ -22,6 +22,41 @@ $city = $_POST['city'];
 $district = $_POST['district'];
 $street = $_POST['street'];
 $buildingNo = $_POST['buildingNo'];
+// all img info ------
+$image=$_FILES['image'];
+$imgName=$_FILES['image']['name'];
+$imgTmpName=$_FILES['image']['tmp_name'];
+$imgSize=$_FILES['image']['size'];
+$imgError=$_FILES['image']['error'];
+$imgType=$_FILES['image']['type'];
+$upload_directory="images/";
+$TargetPath=time().$imgName;
+//$imgExt= explode('.', $imgName ); // file extention must be img..
+if(move_uploaded_file($_FILES['image']['tmp_name'], $upload_directory.$TargetPath)){    
+    $QueryInsertFile="INSERT INTO parent SET parent_image='$TargetPath'"; //but didnt sent.
+    // Write Mysql Query Here to insert this $QueryInsertFile   .                   
+  }
+//$imgActulExt= strtolower(end($imgExt)); // to get the seconed data from imgext, using end for the secnd part
+
+$allowed=array('jpg','jpeg','png','pdf');
+
+if(in_array($imgActulExt,$allowed) ){
+if($imgError=== 0){
+if($imgSize < 1000000){
+$imgNameNew= uniqid('', true).".".$imgActulExt; //to prevent over writning another user's photo if they had same name! and to add the extention back
+/*$imgDestination='C:\Users\awaxh\Downloads\New folder\htdocs\Oun-Project\images';//.$imgNameNew;
+move_uploaded_file($imgTmpName,$imgDestination); //from , to
+//header("Location: RegisterParent.php? uploadsucess");*/
+}else{
+    echo "Your file is too big!";  
+}
+}else{
+    echo "there was an error uploading your file!";
+}
+}else{
+    echo "you can not upload this type of files! try to upload only(jpg, jpeg, png, pdf)";
+}
+
 
 if (isset($_POST['image'])) {
     $parentImage = $_POST['image'];
@@ -46,7 +81,7 @@ if (mysqli_num_rows($result) > 0) {
 // Start Registering
 if (count($errors) == 0) {
     $password = md5($password1); //encrypting password by using md5()
-    $query = "INSERT INTO parent (`name`,`password`,`email`,`city`,`district`,`street`,`buildingNo`,`phoneNo`) VALUES ('$username', '$password', '$email', '$city', '$district', '$street', '$buildingNo', '$phone')";
+    $query = "INSERT INTO parent (`parent_image`,`name`,`password`,`email`,`city`,`district`,`street`,`buildingNo`,`phoneNo`) VALUES ('$TargetPath','$username', '$password', '$email', '$city', '$district', '$street', '$buildingNo', '$phone')";
     $result = mysqli_query($con, $query);
     $affected = mysqli_affected_rows($con);
 
