b3b0ade0a32d840c8746202d5aebbc57f2c6c787
deemaalh <dddeema@gmail.com>
2022-11-08 05:46:01 +0300

    time conflict "not sure if it works"

--- sendOffer.php
+++ sendOffer.php
@@ -12,8 +12,13 @@ $reqId = $_GET['id'];
 
 if( isset($_GET["sent"]) && $_GET["price"]!="" )
 $price= $_GET["price"];
+$start = $_GET["start_time"];
+$end = $_GET["end_time"];
+$date = $_GET["date"];
 $query = "INSERT INTO offer (babysitter_id, request_id, price, status)
-VALUES ($sitterId , $reqId , $price ,'p')" ;
+VALUES ($sitterId , $reqId , $price ,'p') 
+WHERE request_id NOT IN 
+( SELECT request_id FROM request WHERE start_time='$start' AND end_time='$end' AND date='$date'"  ;
 
 $q=mysqli_query($con , $query);
 mysqli_close($con);
